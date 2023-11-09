7ca6ff58ebce7ad1700c3656ccfe9bd48c672f2b
Viet Hoang <hoangproo2624@gmail.com>
2023-11-09 08:57:25 +0700

    config-project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +15,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider></body>
     </html>
   )
 }
